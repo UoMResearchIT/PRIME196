d877f52aa40779ef6712098aa7ed7aa355ad35bf
Martin HA <martin.herrerias@gmail.com>
2024-03-20 16:58:40 +0000

    Files originally in PreclinicalMRI/studies

---
+++ OE_IR_DCE_VFA.py
@@ -0,0 +1,15 @@
+'''_summary_
+
+_extended_summary_
+'''
+pre_processes = [
+	    "OE_IR_T1_mapping",
+        "DCE_VFA_T1_mapping",
+        "OE_deltaR1",
+        "DCE_deltaCt",
+        "DCE_ETM",
+		#"OE_DCE_hypoxia_mapping"
+	]
+post_processes = [
+	"OE_DCE_summary"
+	]
